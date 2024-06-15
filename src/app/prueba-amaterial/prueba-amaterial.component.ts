a89b7efa6a1c44dec8a4d23d67b61b26daa7f65c
AngelSantos2912 <angelleonardo975533406@gmail.com>
2024-06-15 12:02:08 -0500

    añadir material

--- src/app/prueba-amaterial/prueba-amaterial.component.ts
+++ src/app/prueba-amaterial/prueba-amaterial.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import { MaterialModule } from '../angular-material/material/material.module';
+//-------------------------------------------------------------------//
 
+
+//----------------------------------------------------------------//
 @Component({
   selector: 'app-prueba-amaterial',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MaterialModule],
   templateUrl: './prueba-amaterial.component.html',
   styleUrl: './prueba-amaterial.component.css'
 })
 export class PruebaAmaterialComponent {
 
 }
+//---------------------------------------------------//
+
+
